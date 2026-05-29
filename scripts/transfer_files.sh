d5d221be9fd3a6124d501a7563d59f791894a1cc
LqS <lqs@lqs-pc.com>
2026-05-29 10:42:40 -0300

    Criação de configuração de network: utiliza o ssh_agent de cada ambiente automaticamente no script script/transfer_files.sh.

--- scripts/transfer_files.sh
+++ scripts/transfer_files.sh
@@ -3,6 +3,9 @@
 # Consultar documentação na seção de libs.
 source "../libs/pathing.sh"
 
+# Consultar documentação na seção de configs.
+source "../config/network_config"
+
 # Transferência de arquivos que não constam no destino (servidor 1)
-rsync -rv "../Media/Filmes_Saida" "servidor_2:/mnt/16tb/Audio_Separation_Pipeline/Media"
+rsync -rv "../Media/Filmes_Saida" "$SSH_AGENT:/mnt/16tb/Audio_Separation_Pipeline/Media"
 
