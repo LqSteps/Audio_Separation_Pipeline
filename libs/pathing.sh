3fa058505c9dd905d5b40d9936654e79687e958c
LqS <lqs@lqs-pc.com>
2026-05-29 10:33:03 -0300

    Início - Configurações de Sistema
    Criação do diretório configs/
    Criação da primeira configuração, configs/ffmpeg_config, que utiliza codec h624_nvenc na produção (servidor 2) e libx264 no ambiente dev local.
    
    Adaptação do script de reencode para utilizar configuração do ffmpeg.

--- libs/pathing.sh
+++ libs/pathing.sh
@@ -8,11 +8,14 @@ MEDIA_DIR="$BASE_DIR/Media"
 INPUT_DIR="$MEDIA_DIR/Filmes_Entrada"
 OUTPUT_DIR="$MEDIA_DIR/Filmes_Saida"
 SERVICE_DIR="$BASE_DIR/services"
+CONFIG_DIR="$BASE_DIR/congig"
 
-help=$(echo -e "\$BASE_DIR = Diretório Raiz. \ 
+help=$(echo -e "\$BASE_DIR = Diretório Raiz.\ 
 	\n\$SCRIPT_DIR = Diretório de Scripts. \ 
 	\n\$LIB_DIR = Diretório de Bibliotecas. \ 
 	\n\$LOG_DIR = Diretório de Logs. \ 
         \n\$MEDIA_DIR = Diretório com subdiretórios de I/O de Mídia. \ 
 	\n\$INPUT_DIR = Diretório de Entrada de Vídeos. \ 
-	\n\$OUTPUT_DIR = Diretório de Saída de Vídeos.")
+	\n\$OUTPUT_DIR = Diretório de Saída de Vídeos. \ 
+	\n\$SERVICE_DIR = Diretório de Serviços do Systemd \ 
+	\n\$CONFIG_DIR = Diretório de configurações.") 
