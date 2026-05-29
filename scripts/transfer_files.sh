950a6e05bc0be40ca27bee7431836dbc33519070
LqS <lqs@lqs-pc.com>
2026-05-29 09:44:27 -0300

    remoção de script duplicado vazio na raiz do projeto, atualização do script/transfer_files.sh com váriaveis de ambiente locais.

--- scripts/transfer_files.sh
+++ scripts/transfer_files.sh
@@ -4,5 +4,5 @@
 source "../libs/pathing.sh"
 
 # Transferência de arquivos que não constam no destino (servidor 1)
-rsync -rv "../Media/Filmes_Saida" "root@157.180.2.67:/mnt/16tb/Audio_Separation_Pipeline/Media"
+rsync -rv "../Media/Filmes_Saida" "servidor_2:/mnt/16tb/Audio_Separation_Pipeline/Media"
 
