eecd2aa277fc6a4b3b6b0949b9cf9ce0bbfe563b
LqSteps <pedropbrangel@gmail.com>
2026-06-05 06:32:05 -0300

    Pipeline agora roda opcionalmente, mas preferencialmente em Docker (APENAS NA PRODUÇÂO), todos arquivos adicionados/modificados são para adequação do sistema à este modelo

--- run_info/full_info.sh
+++ run_info/full_info.sh
@@ -5,5 +5,5 @@ tmux split-window -v
 tmux split-window -h -t main:0.0
 tmux send-keys -t main:0.0 'bash queue.sh' Enter
 tmux send-keys -t main:0.1 'bash progress.sh' Enter
-tmux send-keys -t main:0.2 'watch -t journalctl -fu demucs_pipeline.service' Enter
+tmux send-keys -t main:0.2 'docker compose logs -f' Enter
 tmux attach -t main
--- scripts/start_pipeline.sh
+++ scripts/start_pipeline.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 # Descrição: Executa pipeline completo respeitando a ordem dos scripts do projeto, evita-se paralelismo para scripts não receberem inputs inacabados de passos anteriores.
 readonly ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-source "$ROOT_DIR/libs/pathing.sh"
-"$SCRIPT_DIR/std_reencode.sh"
-"$SCRIPT_DIR/channel_layout_id.sh"
-"$SCRIPT_DIR/extract_mono_stereo.sh"
-"$SCRIPT_DIR/split_multichannel.sh"
-"$SCRIPT_DIR/demucs_split.sh"
-"$SCRIPT_DIR/transfer_files.sh"
-sleep 120
+
+while true; do
+	source "$ROOT_DIR/libs/pathing.sh"
+	"$SCRIPT_DIR/std_reencode.sh"
+	"$SCRIPT_DIR/channel_layout_id.sh"
+	"$SCRIPT_DIR/extract_mono_stereo.sh"
+	"$SCRIPT_DIR/split_multichannel.sh"
+	"$SCRIPT_DIR/demucs_split.sh"
+	"$SCRIPT_DIR/transfer_files.sh"
+	sleep 120
+done
